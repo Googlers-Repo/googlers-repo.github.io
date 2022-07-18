6746d7618e4cff2c662b1273f1bc59a2024faa71
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-07-18 15:50:24 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -8,18 +8,18 @@ REPO_NAME = "Googlers-Magisk-Repo"
 REPO_TITLE = "Googlers Magisk Repo"
 REPO_WEBSITE = "https://dergoogler.com/repo"
 REPO_SUPPORT = "https://t.me/The_Googler"
-REPO_DONATE = "null"
-REPO_SUBMIT_MODULE = "null"
+REPO_DONATE = None
+REPO_SUBMIT_MODULE = None
 
 # Skeleton for the repository
 meta = {
+    "last_update": "",
     "name": REPO_TITLE,
     "website": REPO_WEBSITE,
     "support": REPO_SUPPORT,
-    "donate": REPO_DONATE, 
+    "donate": REPO_DONATE,
     "submitModule": REPO_SUBMIT_MODULE,
-    "modules": [],
-    "last_update": ""
+    "modules": []
 }
 
 # Initialize the GitHub objects
@@ -35,38 +35,48 @@ for repo in repos:
     # It is possible that module.prop does not exist (meta repo)
     try:
         # Parse module.prop into a python object
-        moduleprop_raw = repo.get_contents("module.prop").decoded_content.decode("UTF-8")
+        moduleprop_raw = repo.get_contents(
+            "module.prop").decoded_content.decode("UTF-8")
         moduleprop = {}
         for line in moduleprop_raw.splitlines():
             if "=" not in line:
                 continue
             lhs, rhs = line.split("=", 1)
             moduleprop[lhs] = rhs
-        
+
+        def getProp(name):
+            try:
+                return moduleprop[name]
+            except:
+                return None
+
         # Isn't that way better?
         details = {
-            "name": moduleprop["name"],
-            "version": moduleprop["version"],
-            "versionCode": moduleprop["versionCode"],
-            "author": moduleprop["author"],
-            #"minApi": moduleprop["minApi"],
-            #"maxApi": moduleprop["maxApi"],
-            #"minMagisk": moduleprop["minMagisk"],
-            #"needRamdisk": moduleprop["needRamdisk"],
-            #"support": moduleprop["support"],
-            #"donate": moduleprop["donate"],
-            #"config": moduleprop["config"],
-            #"changeBoot": moduleprop["changeBoot"]
+            "name": getProp("name"),
+            "author": getProp("author"),
+            "version": getProp("version"),
+            "versionCode": getProp("versionCode"),
+            "description": getProp("description"),
+            "foxprops": {
+                "minApi": getProp("minApi"),
+                "maxApi": getProp("maxApi"),
+                "minMagisk": getProp("minMagisk"),
+                "needRamdisk": getProp("needRamdisk"),
+                "support": getProp("support"),
+                "donate": getProp("donate"),
+                "config": getProp("config"),
+                "changeBoot": getProp("changeBoot")
+            }
         }
 
         module = {
             "id": moduleprop["id"],
             "last_update": int(repo.updated_at.timestamp() * 1000),
             # "prop_url": f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/module.prop",
-            "prop_url": details,
             "zip_url": f"https://github.com/{repo.full_name}/archive/{repo.default_branch}.zip",
             "notes_url": f"https://raw.githubusercontent.com/{repo.full_name}/{repo.default_branch}/README.md",
-            "stars": int(repo.stargazers_count)
+            # "stars": int(repo.stargazers_count),
+            "prop_url": details,
         }
 
         # Append to skeleton
@@ -75,5 +85,4 @@ for repo in repos:
         continue
 
 # Return our final skeleton
-print(json.dumps(meta, indent=4, sort_keys=True))
-
+print(json.dumps(meta, indent=4))
