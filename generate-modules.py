6d1df40f23dca1ec1e09192a1c2fcf9ba1148c91
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2023-09-18 04:46:09 +0200

    Update generate-modules.py

--- generate-modules.py
+++ generate-modules.py
@@ -5,12 +5,12 @@ from github import Github
 from datetime import datetime
 
 # Configuration
-REPO_NAME = os.environ['REPO_NAME'] 
-REPO_TITLE = os.environ['REPO_TITLE']
-REPO_WEBSITE = os.environ['REPO_WEBSITE']
-REPO_SUPPORT = os.environ['REPO_SUPPORT']
-REPO_DONATE = os.environ['REPO_DONATE']
-REPO_SUBMIT_MODULE = os.environ['REPO_SUBMIT_MODULE']
+REPO_NAME = os.environ['REPO_NAME'] or Null
+REPO_TITLE = os.environ['REPO_TITLE'] or Null
+REPO_WEBSITE = os.environ['REPO_WEBSITE'] or Null
+REPO_SUPPORT = os.environ['REPO_SUPPORT'] or Null
+REPO_DONATE = os.environ['REPO_DONATE'] or Null
+REPO_SUBMIT_MODULE = os.environ['REPO_SUBMIT_MODULE'] or Null
 
 # Initialize the GitHub objects
 g = Github(os.environ['GIT_TOKEN'])
